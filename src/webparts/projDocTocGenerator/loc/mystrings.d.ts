049537903c97e6257c23aa19280574f54a087900
Dmitriy Marach <marach.dv@mt.modul.tech>
2021-09-17 12:53:48 +0300

    Init Commit

---
+++ src/webparts/projDocTocGenerator/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IProjDocTocGeneratorWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ProjDocTocGeneratorWebPartStrings' {
+  const strings: IProjDocTocGeneratorWebPartStrings;
+  export = strings;
+}
